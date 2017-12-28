24d70cf59154056eae3460f61467d7aa2b4eedd4
andiwawank <andifirwansyah250@gmail.com>
2017-12-28 19:10:42 +0700

    Update main.cpp

--- absensi/main.cpp
+++ absensi/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int main(){
     char nama[5][50]={
-        "Andis",
-        "Indri",
-        "Wiwi",
-        "Rini",
+        "Andi",
+        "Kondo",
+        "Gigo",
+        "Loser",
         "Wawan"
     };
     char nim[5][50]={
@@ -39,24 +39,78 @@ int main(){
     
     switch(menu) {
         case 1:
+            printf("\n*ABSENSI TEKNIK INFORMATIKA*\n");
             for (i=0; i<5; i++){
                 printf("\n NAMA : %s ",nama[i]);
                 printf("\n NIM : %s ",nim[i]);
                 printf("\n Hadir / Tidak ? : ");
                 scanf("%s",&kehadiran.absensi[i]);
             }
+            printf("\n*==============HASIL ABSENSI MAHASISWA TEKNIK INFORMATIKA==============*\n");
+            for (i=0; i<5; i++) {
+                printf("\n NAMA : %s ",nama[i]);
+                printf("\n NIM : %s ",nim[i]);
+                printf("\n Kehadirannya : %s \n",kehadiran.absensi[i]);
+        
+            }
             break;
         case 2:
-            printf("Mahasiswa dengan Nama = Indri");
+            printf("\n*ABSENSI SISTEM INFORMASI*\n");
+//             for (i=0; i<5; i++){
+//                 printf("\n NAMA : %s ",nama[i]);
+//                 printf("\n NIM : %s ",nim[i]);
+//                 printf("\n Hadir / Tidak ? : ");
+//                 scanf("%s",&kehadiran.absensi[i]);
+//             }
+             printf("\n*==============HASIL ABSENSI MAHASISWA TEKNIK INFORMATIKA==============*\n");
+//             for (i=0; i<5; i++) {
+//                 printf("\n NAMA : %s ",nama[i]);
+//                 printf("\n NIM : %s ",nim[i]);
+//                 printf("\n Kehadirannya : %s \n",kehadiran.absensi[i]);
+        
+//             }
             break;
         default:
             break;
     }
-    for (i=0; i<5; i++) {
-        printf("\n NAMA : %s ",nama[i]);
-        printf("\n NIM : %s ",nim[i]);
-        printf("\n Kehadirannya : %s \n",kehadiran.absensi[i]);
-        
+    switch(menuDet) { //Bagian Menampilkan detail mahasiswa
+        case 1:
+            printf("\n*==============DETAIL MAHASISWA TI==============*\n");
+            for (i=0; i<5; i++) {
+                printf("\n-----------------\n");
+                printf("|");
+                printf("\n| NAMA         : %s ",nama[i]);
+                printf("\n| NIM          : %s ",nim[i]);
+                if(kehadiran.absensi[i] != 0){
+                    printf("\n| Keterangan   : Belum ada keterangan!");
+                }else{
+                    printf("\n| Keterangan   : %s",kehadiran.absensi[i]);
+                }
+//                printf("\n| Keterangan   : %s",kehadiran.absensi[i]);
+                printf("\n|");
+
+                
+            }
+            break;
+        case 2:
+            printf("\n*==============DETAIL MAHASISWA SI==============*\n");
+            printf("\nMaaff! data mahasiswa jurusan ini belum diinput \n");
+//            for (i=0; i<5; i++) {
+//                printf("\n-----------------\n");
+            
+//                printf("|");
+//                printf("\n| NAMA         : %s ",nama[i]);
+//                printf("\n| NIM          : %s ",nim[i]);
+//                    if(kehadiran.absensi[i] != 0){
+//                        printf("\n| Keterangan   : Belum ada keterangan!");
+//                    }else{
+//                        printf("\n| Keterangan   : %s",kehadiran.absensi[i]);
+//                }
+//                printf("\n|");
+//            }
+            break;
+        default:
+            break;
     }
 } //main
 
